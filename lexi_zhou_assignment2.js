536dd8d45cf898d55d899bf3852175106d55052e
hardfeeeder <108059962+hardfeeeder@users.noreply.github.com>
2022-07-15 04:27:12 -0400

    Update lexi_zhou_assignment2.js

--- lexi_zhou_assignment2.js
+++ lexi_zhou_assignment2.js
@@ -45,34 +45,26 @@ const second = [
 
 function merge(first, second) {
   const result = [];
-
   for (const item of first) {
     const index = second.findIndex((i) => i.uuid === item.uuid);
-
     if (index !== -1) {
       const { uuid, name, role } = { ...item, ...second[index] };
-
       result.push({ uuid, name, role });
     } else {
       const { uuid, name } = item;
-
       result.push({ uuid, name, role: null });
     }
   }
   for (const item of second) {
     const index = first.findIndex((i) => i.uuid === item.uuid);
-
     if (index !== -1) {
       const { uuid, name, role } = { ...item, ...first[index] };
-
       result.push({ uuid, name, role });
     } else {
       const { uuid, role } = item;
-
       result.push({ uuid, name: null, role });
     }
   }
-
   return result.sort((a, b) => a.uuid - b.uuid);
 }
 
